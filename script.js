d454f7ce9d5b5d987bf8b6aa27535a7dc62ef1bf
Lukasz Brzozko <lukasz9099@gmail.com>
2020-06-26 22:30:09 +0200

    Fix script name

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @include      https://store.steampowered.com/app/*
 // @include      https://store.steampowered.com/agecheck/*
 // @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/index.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/script.js
 // @grant        none
 // ==/UserScript==
 

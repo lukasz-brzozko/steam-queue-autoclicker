45a7885472f9c851cea38d6217cd4508c7952b2c
Lukasz Brzozko <lukasz9099@gmail.com>
2020-06-26 23:07:35 +0200

    Add url fix

--- autoclicker.user.js
+++ autoclicker.user.js
@@ -7,8 +7,8 @@
 // @include      https://store.steampowered.com/explore/*
 // @include      https://store.steampowered.com/app/*
 // @include      https://store.steampowered.com/agecheck/*
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/user.js
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/autoclicker.user.js
 // @grant        none
 // ==/UserScript==
 

2e4040846ce8bd9dcb702e40a7bda2f1c74adc20
Lukasz Brzozko <lukasz9099@gmail.com>
2020-06-26 22:28:54 +0200

    Add meta.js, update and download URL

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Steam kolejka odkryć
 // @namespace    https://steamcommunity.com/id/pr0sto
-// @version      0.1
+// @version      0.1.1
 // @description  Automatyczne przeglądanie kolejki odkryć
 // @author       pr0sto
 // @include      https://store.steampowered.com/explore/*
 // @include      https://store.steampowered.com/app/*
 // @include      https://store.steampowered.com/agecheck/*
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/steam-queue-autoclicker/master/index.js
 // @grant        none
 // ==/UserScript==
 
